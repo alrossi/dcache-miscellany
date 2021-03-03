284106bcb1788414df1ea6aedd4e5aaab6cfe412
Albert Louis Rossi <arossi@fnal.gov>
2021-03-03 14:53:47 -0600

    progressing

--- qos/090.sh
+++ qos/090.sh
@@ -8,5 +8,4 @@ mkdir -p  modules/dcache-qos/src/main/java/org/dcache/qos/services/adjuster/util
 
 cp /Work/fnal/dCache/sandbox/modules/dcache-qos/src/main/java/org/dcache/qos/services/adjuster/util/QoSAdjusterCounters.java modules/dcache-qos/src/main/java/org/dcache/qos/services/adjuster/util
 cp /Work/fnal/dCache/sandbox/modules/dcache-qos/src/main/java/org/dcache/qos/services/adjuster/util/StaticSinglePoolList.java modules/dcache-qos/src/main/java/org/dcache/qos/services/adjuster/util
-cp /Work/fnal/dCache/sandbox/modules/dcache-qos/src/main/java/org/dcache/qos/services/verifier/util/DegenerateSelectionStrategy.java modules/dcache-qos/src/main/java/org/dcache/qos/services/adjuster/util
-
+cp /Work/fnal/dCache/sandbox/modules/dcache-qos/src/main/java/org/dcache/qos/services/adjuster/util/DegenerateSelectionStrategy.java modules/dcache-qos/src/main/java/org/dcache/qos/services/adjuster/util
