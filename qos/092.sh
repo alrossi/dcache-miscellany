c3deaddf9c4af94c2b57e6261f6634f8a13a5871
Albert Louis Rossi <arossi@fnal.gov>
2021-03-31 11:47:40 -0500

    remove adjuster patches

--- qos/092.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-#
-# (9) QoS Adjuster 2 Client, Receiver
-#
-
-cp /Work/fnal/dCache/sandbox/modules/dcache-qos/src/main/java/org/dcache/qos/local/clients/LocalQoSAdjustmentClient.java modules/dcache-qos/src/main/java/org/dcache/qos/local/clients
-cp /Work/fnal/dCache/sandbox/modules/dcache-qos/src/main/java/org/dcache/qos/remote/receivers/QoSAdjustmentReceiver.java modules/dcache-qos/src/main/java/org/dcache/qos/remote/receivers
